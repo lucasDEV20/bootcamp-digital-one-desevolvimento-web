c9f15a20055389055389b67c178e411de45a86fb
lucasDEV20 <lucasgoias11@gmail.com>
2022-01-29 15:14:36 -0300

    remanejando projeto

---
+++ bootcamp-digital-one-desevolvimento-web/JavaScript/aula-JavaScript/js/main.js
@@ -0,0 +1,31 @@
+var fruta = { nome: "maça", cor: "vermelha" }
+console.log(fruta);
+
+
+//var lista = ["maça", "pera", "laranja"];
+//lista.push("uva");
+//ista.pop();
+
+//console.log(lista.length);
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" | "));
+
+
+//var nome = "lucas araujo "; //string
+//var n1 = 5; //inteiro
+//var n2 = 3; //inteiro
+//var frase = "japão e o melhor time do mundo"; //String
+
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+
+//onsole.log(nome);
+//console.log(n1 * n2);
+//console.log(frase.toLowerCase());
+
+//toLocaleUpperCase = transorma tudo em caixa alta , toda string que
+//for relacionada tera alteraçao para maiusculo
+//toLowerCase = transorma tudo em letra minuscula
+
+//alert(frase.replace("japão", "Brasil"));
